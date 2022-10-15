b85d79cdda5a587518d1632fb274737bd957a3b2
Kunwar Nir <knir45@gmail.com>
2022-10-14 23:27:20 -0400

    basic textbook class

---
+++ textbook.py
@@ -0,0 +1,12 @@
+class Textbook:
+
+    def __init__(self, textbook):
+        self.textbook = textbook
+    
+    def getGlossary():
+        return -1
+
+    def getQuestionBank():
+        return -1
+    
+        
