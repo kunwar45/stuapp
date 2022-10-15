810e04f2a9c6f7da202941e65c0c714524f63127
Kunwar Nir <knir45@gmail.com>
2022-10-15 12:56:59 -0400

    made changes

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,16 @@ function NavBar(){
               <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
             </li>
           </ul>
+          <div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Drop
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </div>
         </div>
       </nav>
     </div>
