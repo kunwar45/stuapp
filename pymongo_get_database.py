4eede4d794e89e85b8cf0138f49677a8562f83e9
Kunwar Nir <knir45@gmail.com>
2022-10-16 04:31:02 -0400

    added mongo query things

--- pymongo_get_database.py
+++ pymongo_get_database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 def get_database():
  
    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-   CONNECTION_STRING = "mongodb+srv://user:pass@cluster.mongodb.net/myFirstDatabase"
+   CONNECTION_STRING = "mongodb+srv://cluster0.2oxihpn.mongodb.net/myFirstDatabase"
  
    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
    client = MongoClient(CONNECTION_STRING)
