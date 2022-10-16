10986d22f604ed9f97248689c5377c55f2a345bd
Kunwar Nir <knir45@gmail.com>
2022-10-16 04:36:03 -0400

    edited mongo query

--- pymongo_test_query.py
+++ pymongo_test_query.py
@@ -3,9 +3,9 @@ from pymongo_get_database import get_database
 dbname = get_database()
  
 # Create a new collection
-collection_name = dbname["user_1_items"]
+basicDB = dbname["user_1_items"]
  
-item_details = collection_name.find()
+item_details = basicDB.find()
 for item in item_details:
    # This does not give a very readable output
    print(item)
