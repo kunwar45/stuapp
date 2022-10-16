e7ec9561bed7ffdccd1bc46c20d4a6b6b2e7951f
Kunwar Nir <knir45@gmail.com>
2022-10-16 04:25:19 -0400

    fixed naming

--- pymongo_get_database.py
+++ pymongo_get_database.py
@@ -8,7 +8,7 @@ def get_database():
    client = MongoClient(CONNECTION_STRING)
  
    # Create the database for our example (we will use the same database throughout the tutorial
-   return client['user_shopping_list']
+   return client['basicDB']
   
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":   
