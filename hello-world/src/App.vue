8620688b8ab2245b7d7cd2c5b24fec1732086d00
Hassan Ali <thehassanali2@gmail.com>
2022-10-16 07:21:00 -0400

    updated vue2

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
